883440ad01cd97817d1e8bfb4299be6799fe619c
ACER <robiyatuladawiyahh479@gmail.com>
2024-09-07 22:54:27 +0700

    Add PenerapanString.kt

--- src/main/kotlin/PenerapanString.kt
+++ src/main/kotlin/PenerapanString.kt
@@ -1,16 +1,11 @@
 fun main(args: Array<String>) {
-    // Character occurrence in a sentence analysis
-    // the string that we want to analyze
-    var s =
-        "A programmer gets stuck in the shower because the instructions on the shampoo were: Lather, Wash, and Repeat."
+    var s = "A programmer gets stuck in the shower because the instructions on the shampoo were: Lather, Wash, and Repeat."
     println(s)
     s = s.toLowerCase()
 
-    // counters initialization
     var vowelCount = 0
     var consonantCount = 0
 
-    // definition of character groups
     val vowels = "aeiouy"
     val consonants = "bcdfghjklmnpqrstvwxz"
 
