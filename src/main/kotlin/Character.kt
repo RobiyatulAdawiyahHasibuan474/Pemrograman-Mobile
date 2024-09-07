47a95a7d57ae0aec7bf8a73a51705d75ce7b839e
ACER <robiyatuladawiyahh479@gmail.com>
2024-09-07 22:13:53 +0700

    Add Character(Char).kt

---
+++ src/main/kotlin/Character.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    var charA : Char = 'A'
+
+    println(charA.isUpperCase())
+    println(charA.isLowerCase())
+    println(charA.isDigit())
+    println(charA.isLowerCase())
+
+    val strA: String = charA.toString()
+    println("Kini charA sudah menjadi String = " +strA)
+
+}
